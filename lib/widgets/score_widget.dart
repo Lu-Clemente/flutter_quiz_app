60d3c9fd6c3f5994c1c4134e76df40ddd497f457
Luciene Clemente <luciene.clemente@unifei.edu.br>
2023-01-23 17:04:35 -0300

    feat: Score showcase Widget created

---
+++ lib/widgets/score_widget.dart
@@ -0,0 +1,35 @@
+import 'package:first_app/theme/colors.dart';
+import 'package:flutter/material.dart';
+
+class Score extends StatelessWidget {
+  final int totalScore;
+
+  Score({required this.totalScore});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text("Total Score",
+            style: TextStyle(
+                color: themedPrimary,
+                fontSize: 20,
+                fontWeight: FontWeight.w500)),
+        Container(
+          height: 70,
+          width: 180,
+          margin: const EdgeInsets.only(top: 10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(color: themedPrimary, width: 2.5)),
+          child: Text(totalScore.toString(),
+              style: const TextStyle(
+                  color: themedPrimary,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w700)),
+        )
+      ],
+    );
+  }
+}
